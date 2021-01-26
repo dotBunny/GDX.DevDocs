e572cb5f819c8c5b86a85367a2023313fe82de86
Matthew Davey <matthew.davey@dotbunny.com>
2021-01-26 16:42:47 -0500

    Adapt changelog to have yaml header.

--- scripts/steps/changelog.bat
+++ scripts/steps/changelog.bat
@@ -1,5 +1,10 @@
 :: These scripts are setup to be pathed to the workspace used internally for GDX development.
 :: They almost certainly will not have the correct paths for anyone else.
 
-:: Copy over most recent changelog
-copy /Y %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\CHANGELOG.md %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\changelog.md 
+set TARGET=%~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\changelog.md
+
+echo --- > %TARGET%
+echo _disableBreadcrumb: true >> %TARGET%
+echo _disableContribution: true >> %TARGET%
+echo --- >> %TARGET%
+type %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\CHANGELOG.md >> %TARGET%
