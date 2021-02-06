c795fcf60c57e3fd19e6d7ce6e65cce5abefbd5d
Matthew Davey <matthew.davey@dotbunny.com>
2021-02-06 08:38:39 -0500

    Rename Tools folder to External

--- scripts/steps/step-build.bat
+++ scripts/steps/step-build.bat
@@ -2,4 +2,4 @@
 :: They almost certainly will not have the correct paths for anyone else.
 
 :: Build content
-%~dp0..\..\..\Tools\docfx\docfx.exe %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\docfx.json --build
+%~dp0..\..\..\External\docfx\docfx.exe %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\docfx.json --build
--- scripts/steps/step-serve.bat
+++ scripts/steps/step-serve.bat
@@ -1,3 +1,3 @@
 :: These scripts are setup to be pathed to the workspace used internally for GDX development.
 :: They almost certainly will not have the correct paths for anyone else.
-%~dp0..\..\..\Tools\docfx\docfx.exe %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\docfx.json --serve
+%~dp0..\..\..\External\docfx\docfx.exe %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\docfx.json --serve
