0a83d109343ac66380f416f9d87dfc28dc4c0546
Matthew Davey <matthew.davey@dotbunny.com>
2021-03-29 22:41:35 -0400

    Smarter XMLdocs generation

--- scripts/steps/step-xmldocs.bat
+++ scripts/steps/step-xmldocs.bat
@@ -2,4 +2,10 @@
 :: They almost certainly will not have the correct paths for anyone else.
 
 :: Build content
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /p:Configuration=Debug /p:GenerateDocumentation=true /p:WarningLevel=0 /p:DocumentationFile="%~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.xml" %~dp0..\..\..\Projects\000_Development\GDX.csproj
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"^
+ /p:Configuration=Debug^
+ /p:GenerateDocumentation=true^
+ /p:WarningLevel=0^
+ /p:DefineConstants="GDX_ADDRESSABLES;GDX_BURST;GDX_MATHEMATICS;GDX_PLATFORMS;GDX_VISUALSCRIPTING"^
+ /p:DocumentationFile="%~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.xml"^
+  %~dp0..\..\..\Projects\000_Development\GDX.csproj
