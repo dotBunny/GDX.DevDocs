dcfa40cc2978643909d9dbbae8c597088bdccd87
Matthew Davey <matthew.davey@dotbunny.com>
2021-04-22 22:25:02 -0400

    Create licensed xml as well

--- scripts/steps/step-xmldocs.bat
+++ scripts/steps/step-xmldocs.bat
@@ -11,3 +11,13 @@
   %~dp0..\..\..\Projects\000_Development\GDX.csproj
 
 %~dp0..\..\tools\XmlDocForBolt\XmlDocForBolt.exe %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.xml
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"^
+ /p:Configuration=Debug^
+ /p:GenerateDocumentation=true^
+ /p:WarningLevel=0^
+ /p:DefineConstants="GDX_ADDRESSABLES;GDX_BURST;GDX_MATHEMATICS;GDX_PLATFORMS;GDX_VISUALSCRIPTING"^
+ /p:DocumentationFile="%~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.Licensed.xml"^
+  %~dp0..\..\..\Projects\000_Development\GDX.Licensed.csproj
+
+%~dp0..\..\tools\XmlDocForBolt\XmlDocForBolt.exe %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.Licensed.xml
