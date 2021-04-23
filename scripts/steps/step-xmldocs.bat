af4c48f85395c1f9233cff689af1a2340b32650b
Matthew Davey <matthew.davey@dotbunny.com>
2021-04-23 08:59:45 -0400

    Update for singular library but ifdef

--- scripts/steps/step-xmldocs.bat
+++ scripts/steps/step-xmldocs.bat
@@ -6,18 +6,8 @@
  /p:Configuration=Debug^
  /p:GenerateDocumentation=true^
  /p:WarningLevel=0^
- /p:DefineConstants="GDX_ADDRESSABLES;GDX_BURST;GDX_MATHEMATICS;GDX_PLATFORMS;GDX_VISUALSCRIPTING"^
+ /p:DefineConstants="GDX_LICENSED;GDX_ADDRESSABLES;GDX_BURST;GDX_MATHEMATICS;GDX_PLATFORMS;GDX_VISUALSCRIPTING"^
  /p:DocumentationFile="%~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.xml"^
   %~dp0..\..\..\Projects\000_Development\GDX.csproj
 
 %~dp0..\..\tools\XmlDocForBolt\XmlDocForBolt.exe %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.xml
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"^
- /p:Configuration=Debug^
- /p:GenerateDocumentation=true^
- /p:WarningLevel=0^
- /p:DefineConstants="GDX_ADDRESSABLES;GDX_BURST;GDX_MATHEMATICS;GDX_PLATFORMS;GDX_VISUALSCRIPTING"^
- /p:DocumentationFile="%~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.Licensed.xml"^
-  %~dp0..\..\..\Projects\000_Development\GDX.Licensed.csproj
-
-%~dp0..\..\tools\XmlDocForBolt\XmlDocForBolt.exe %~dp0..\..\..\Projects\000_Development\Assets\com.dotbunny.gdx\.docfx\GDX.Licensed.xml
