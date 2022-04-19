0465e5547ded751bd6dd1172c4950f4121d0259d
Matthew Davey <matthew.davey@dotbunny.com>
2022-04-19 14:43:41 -0400

    Better folder cleaning

--- scripts/steps/step-clean.bat
+++ scripts/steps/step-clean.bat
@@ -3,8 +3,12 @@
 
 echo [1m[36m^> Clean Old Files[0m
 
+rmdir %~dp0..\..\..\Projects\GDX_Development\obj\xdoc /s /q
+
 rmdir %~dp0..\..\..\Projects\GDX_Development\Packages\com.dotbunny.gdx\.docfx\obj /s /q
 rmdir %~dp0..\..\..\Projects\GDX_Development\Packages\com.dotbunny.gdx\.docfx\_site /s /q
 
+del /s /f /q  %~dp0..\..\..\Projects\GDX_Development\Packages\com.dotbunny.gdx\.docfx\api\*.yml
+del /s /f /q  %~dp0..\..\..\Projects\GDX_Development\Packages\com.dotbunny.gdx\.docfx\api\.manifest
 del /s /f /q %~dp0..\..\..\Projects\GDX_Development\Packages\com.dotbunny.gdx\.docfx\templates\gdx\partials\footer.tmpl.partial
 del /s /f /q %~dp0..\..\..\Projects\GDX_Development\Packages\com.dotbunny.gdx\.docfx\changelog.md
