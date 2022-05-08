ec608bf8b8f304bf101cbc1213c7ef91c2c8b4c7
dotBunny-BuildAgent <support@dotbunny.com>
2022-05-08 17:02:46 -0400

    Generated documentation at b2c8ab2.

--- docs/styles/main.js
+++ docs/styles/main.js
@@ -1,1 +1,37 @@
-// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Sometimes you have to use the right tool for the right job.
+$( document ).ready(function() {
+
+    var pathname = window.location.pathname;
+    var origin   = window.location.origin;
+
+    // Select current branch
+    if(origin.includes("localhost:8080"))
+    {
+        $("#environment").append('<optgroup label="Environment"><option value="localhost">localhost</option></optgroup>');
+        $("#environment").val("localhost")
+    }
+    else if(origin.includes("gdx-dev.dotbunny.com"))
+    {
+        $("#environment").val("dev")
+    }
+    else
+    {
+        $("#environment").val("main")
+    }
+
+    // Setup change event for branch selection
+    $("#environment").change(() => {
+        switch ( $("#environment").val()) {
+
+            case "localhost":
+                window.location.href = "http://localhost:8080".concat(pathname);
+                break;
+            case "main":
+                window.location.href = "http://gdx.dotbunny.com".concat(pathname);
+                break;
+            case "dev":
+                window.location.href = "http://gdx-dev.dotbunny.com".concat(pathname);
+                break;
+        }
+    });
+});
